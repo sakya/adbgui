21476d0c68f67f681a32d2e5868f6a5ebdad2ba2
Paolo Iommarini <paolo.iommarini@xenialab.it>
2024-07-23 15:14:07 +0200

    Set current directory on program startup

--- adbgui/Program.cs
+++ adbgui/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.IO;
 using Projektanker.Icons.Avalonia;
 using Projektanker.Icons.Avalonia.FontAwesome;
 
@@ -11,8 +12,16 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // Set current directory to the one containing the executable
+        var exeDir = Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly()?.Location);
+        if (exeDir != null)
+            Directory.SetCurrentDirectory(exeDir);
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp() {
